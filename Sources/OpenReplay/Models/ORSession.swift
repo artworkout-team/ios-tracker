61ab197c23c79b66d799eaa0c83bbf00698934d1
levochkaa <levochkaaa@icloud.com>
2024-09-16 14:39:01 +0400

    lowercase android

--- Sources/OpenReplay/Models/ORSession.swift
+++ Sources/OpenReplay/Models/ORSession.swift
@@ -50,7 +50,7 @@ class ORSessionRequest: NSObject {
             "timezone": getTimezone(),
             "width": UInt64(UIScreen.main.bounds.width),
             "height": UInt64(UIScreen.main.bounds.height),
-            "platform": "Android"
+            "platform": "android"
         ]
         callAPI(completion: completion)
     }
